1d4f2b1a0065c4ed472a42c99886e6d2f2b72f52
lehongphongxm <lehongphonghhxm@gmail.com>
2022-01-03 01:20:26 +0700

    giao dien

---
+++ lib/pages/home/widgets/book_staggered_gridview.dart
@@ -0,0 +1,36 @@
+import 'package:cuoiky_readbooks/models/book.dart';
+import 'package:cuoiky_readbooks/pages/home/widgets/book_item.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_staggered_grid_view/flutter_staggered_grid_view.dart';
+
+class BookStaggeredGridView extends StatelessWidget {
+  final int selected;
+  final PageController pageController;
+  final Function callback;
+  BookStaggeredGridView(this.selected, this.pageController, this.callback,
+      {Key? key})
+      : super(key: key);
+  final bookList = Book.generateBooks();
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: PageView(
+        controller: pageController,
+        onPageChanged: (index) => callback(index),
+        children: [
+          StaggeredGridView.countBuilder(
+              shrinkWrap: true,
+              physics: const ScrollPhysics(),
+              crossAxisCount: 4,
+              crossAxisSpacing: 16,
+              mainAxisSpacing: 16,
+              itemCount: bookList.length,
+              itemBuilder: (_, index) => BookItem(bookList[index]),
+              staggeredTileBuilder: (_) => const StaggeredTile.fit(2)),
+          Container()
+        ],
+      ),
+    );
+  }
+}
