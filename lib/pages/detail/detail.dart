62bf493d44067928dd51037b921a6aa9a135ca07
lehongphongxm <lehongphonghhxm@gmail.com>
2022-01-03 01:28:41 +0700

    chi tiet sach

---
+++ lib/pages/detail/detail.dart
@@ -0,0 +1,50 @@
+import 'package:cuoiky_readbooks/constants/colors.dart';
+import 'package:cuoiky_readbooks/models/book.dart';
+import 'package:cuoiky_readbooks/pages/detail/widgets/book_cover.dart';
+import 'package:cuoiky_readbooks/pages/detail/widgets/book_detail.dart';
+import 'package:cuoiky_readbooks/pages/detail/widgets/book_review.dart';
+import 'package:flutter/material.dart';
+
+class DetailPage extends StatelessWidget {
+  final Book book;
+  const DetailPage(this.book, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        backgroundColor: Colors.white,
+        appBar: _buildAppBar(context),
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              BookDetail(book),
+              BookCover(book),
+              BookReview(book),
+            ],
+          ),
+        ));
+  }
+
+  AppBar _buildAppBar(BuildContext context) {
+    return AppBar(
+      backgroundColor: Colors.white,
+      elevation: 0,
+      leading: IconButton(
+        onPressed: () => Navigator.of(context).pop(),
+        icon: const Icon(
+          Icons.arrow_back_ios_outlined,
+          color: kFont,
+        ),
+      ),
+      actions: [
+        IconButton(
+          onPressed: () {},
+          icon: const Icon(
+            Icons.more_horiz_outlined,
+            color: kFont,
+          ),
+        ),
+      ],
+    );
+  }
+}
